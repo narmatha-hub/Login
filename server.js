e17682d41ec2da153c40646eee3e6db0012a7126
narmatha-hub <narmathaelangovan610@gmail.com>
2024-08-26 00:14:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
- document.getElementById('loginForm').addEventListener('submit', function(event) {
-            event.preventDefault();
-            
-            const username = document.getElementById('username').value;
-            const password = document.getElementById('password').value;
-            const messageElement = document.getElementById('message');
-            
-            if (username === 'narmatha' && password === '1234') {
-                messageElement.innerHTML = '<a href="home.html">Go to Dashboard</a>';
-                messageElement.style.color = 'green';
-            } else {
-                messageElement.textContent = 'Invalid username or password.';
-                messageElement.style.color = 'red';
-            }
-        });
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const messageElement = document.getElementById('message');
+    
+    if (username === 'narmatha' && password === '1234') {
+        window.location.href = 'dashboard.html';
+    } else {
+        messageElement.textContent = 'Invalid username or password.';
+    }
+});
+
+          
