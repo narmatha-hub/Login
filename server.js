be019b55282ee564fa6cdc81854248f11250c9d3
narmatha-hub <narmathaelangovan610@gmail.com>
2024-08-25 23:57:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-
-const app = express();
-const port = 3000;
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname))); 
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    
-    if (username === 'narmatha' && password === '1234') {
-        res.sendFile(path.join(__dirname, 'home.html'));
-    } else {
-        res.send('Invalid username or password.');
-    }
-});
-
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
-});
+ document.getElementById('loginForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+            
+            const username = document.getElementById('username').value;
+            const password = document.getElementById('password').value;
+            const messageElement = document.getElementById('message');
+            
+            if (username === 'narmatha' && password === '1234') {
+                messageElement.innerHTML = '<a href="home.html">Go to Dashboard</a>';
+                messageElement.style.color = 'green';
+            } else {
+                messageElement.textContent = 'Invalid username or password.';
+                messageElement.style.color = 'red';
+            }
+        });
